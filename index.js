748b36468e6fbdbc435a0a4b460a16dbb57e5518
Cheng Vue <csalvue@gmail.com>
2019-06-29 01:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const next = function(head, coll) {
 }
 
 const nodeAt = function(idx, ll, coll) {
-  
+
   let name = ll;
-  
+
   for (let i = 0; i < idx; i++) {
     let node = coll[name]
     name = next(node, coll)
